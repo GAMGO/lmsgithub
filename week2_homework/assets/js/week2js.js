4397fbd8fec5325a3411557836aebdb44776de71
Minsu Lim <44716661+GAMGO@users.noreply.github.com>
2025-07-01 17:53:47 +0900

    week2 homwork solved
    2주차 숙제 완벽히 해결

--- week2_homework/assets/js/week2js.js
+++ week2_homework/assets/js/week2js.js
@@ -1,36 +1,40 @@
 const REST_API_KEY = "e219e42c30d4245eb3b2ce072f6cb332";
 const header = {
-    method: 'GET',
-    headers: {
-        Authorization: `KakaoAK ${REST_API_KEY}`
-    }
-}
+  method: "GET",
+  headers: {
+    Authorization: `KakaoAK ${REST_API_KEY}`,
+  },
+};
 // const searching = document.getElementById('enter');
-const query = '트와이스';
-const url = `https://dapi.kakao.com/v2/search/vclip?query=${query}&sort=recency`;
+const query = "트와이스";
+const url = `https://dapi.kakao.com/v2/search/vclip?query=${query}`;
 // var query;
-var result;
 /*searching.addEventListener('click', () => {
     query = document.getElementById('keywords').value;
-
 })*/
 fetch(url, header)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        result = data;
-        showVideo(data.documents, 10, 10);
-
-    })
-    .catch(error => console.error('fetch 실패'.error))
-function showVideo(docs, pos, count) {
-    const ul = document.getElementById('vedioContent')
-    for (var i = pos; i < pos + count; i++) {
-        const li = document.getElementById('li');
-        li.innerHTML = `<video src="${docs[i].thumbnail}" width="400px" height="400px">`;
-        ul.appendChild(li);
-    }
+  .then((response) => {
+    // console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    showVideo(data.documents, 0, 10);
+  })
+  // .catch((error) => console.error("fetch 실패".error));
+  function showVideo(docs, pos, count) {
+    const ul = document.getElementById("videoContent");
+    console.log(docs[0].play_time)
+    for (let i = pos; i < pos + count; i++) {
+      let videoTypes ={};
+      if (docs[i].play_time >= 100) {
+        videoTypes = "Videos"
+      } else {
+        videoTypes = "Shorts";
+      }
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="${docs[i].url}"><img src="${
+      docs[i].thumbnail
+    }"></a><br/><p>${docs[i].datetime}</p><p>${videoTypes}</p>`;
+    ul.appendChild(li);
+  }
 }
-
-
